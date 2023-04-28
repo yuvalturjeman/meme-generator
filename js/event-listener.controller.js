3af240bdb1f2a2175f71d05e05eb663d4e59f843
yuvalturjeman <120467074+yuvalturjeman@users.noreply.github.com>
2023-04-28 20:43:02 +0300

    Add files via upload

---
+++ js/event-listener.controller.js
@@ -0,0 +1,66 @@
+'use strict'
+var gStartPos
+const TOUCH_EVS = ['touchstart', 'touchmove', 'touchend']
+
+
+
+function addListeners() {
+    addMouseListeners()
+    addTouchListeners()
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousedown', onDown)
+    gElCanvas.addEventListener('mousemove', onMove)
+    gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchstart', onDown)
+    gElCanvas.addEventListener('touchmove', onMove)
+    gElCanvas.addEventListener('touchend', onUp)
+}
+
+function onDown(ev) {
+    // Get the ev pos from mouse or touch
+    const pos = getEvPos(ev)
+    console.log('pos', pos)
+    if (!isLineClicked(pos)) return
+  
+    setLineDrag(true)
+    gStartPos = pos
+    document.body.style.cursor = 'grabbing'
+  }
+
+function onMove(ev) {
+    const { isDrag } = getCurrLine()
+
+    if (!isDrag) return
+
+    const pos = getEvPos(ev)
+    const dx = pos.x - gStartPos.x
+    const dy = pos.y - gStartPos.y
+    moveLine(dx, dy)
+    gStartPos = pos
+    renderMeme()
+}
+
+function getEvPos(ev) {
+    let pos = {
+      x: ev.offsetX,
+      y: ev.offsetY,
+    }
+    // Check if its a touch ev
+    if (TOUCH_EVS.includes(ev.type)) {
+      //soo we will not trigger the mouse ev
+      ev.preventDefault()
+      //Gets the first touch point
+      ev = ev.changedTouches[0]
+      //Calc the right pos according to the touch screen
+      pos = {
+        x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+        y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
+      }
+    }
+    return pos
+  }
