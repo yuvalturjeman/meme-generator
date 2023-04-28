601bd92e365288f788e5bd0bf51a6000c3d82298
yuvalturjeman <120467074+yuvalturjeman@users.noreply.github.com>
2023-04-28 20:08:57 +0300

    Update util.service.js

--- js/services/util.service.js
+++ js/services/util.service.js
@@ -1,17 +1,27 @@
-const STORAGE_KEY = 'memes'
-const STORAGE_KEY1 = 'memesInfo'
+'use strict'
 
 
 
 function makeLorem(wordCount = 100) {
-  const words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
+  const words = ['When your code is not functioning after 24 hours of debuging', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
   var txt = ''
   while (wordCount > 0) {
-      wordCount--
-      txt += words[Math.floor(Math.random() * words.length)] + ' '
+    wordCount--
+    txt += words[Math.floor(Math.random() * words.length)] + ' '
   }
   return txt
 }
+
+function makeLoremHe(wordCount = 100) {
+  const words = ['השמיים', 'מעל', 'הנמל', 'היה', 'צבע של הטלוויזיה', 'מכון', 'אל', 'ערוץ מת', '.', 'כל', 'זה קרה', 'פחות או יותר', '.', 'אני', 'שייך', 'הסיפור', 'לאט לאט', 'מאנשים שונים', 'גם', 'בכלליות', 'קורה', 'במקרים כאלה', 'כל פעם', 'זה', 'היה', 'סיפור שונה', '.', 'זה', 'היה', 'תענוג', 'גם', 'בער']
+  var txt = ''
+  while (wordCount > 0) {
+    wordCount--
+    txt += words[Math.floor(Math.random() * words.length)] + ' '
+  }
+  return txt
+}
+
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min)
@@ -23,7 +33,13 @@ function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
   for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
+}
+
+// general actions for document selectors////////
+function documentActions(elName, action, value) {
+  const elSelector = document.querySelector(elName)
+  elSelector[action] = value
 }
