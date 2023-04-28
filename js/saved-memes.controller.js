96e4af5b64fa313426cf8d1b2441ca1295e80922
yuvalturjeman <120467074+yuvalturjeman@users.noreply.github.com>
2023-04-28 20:23:18 +0300

    Add files via upload

---
+++ js/saved-memes.controller.js
@@ -0,0 +1,37 @@
+'use strict'
+const STORAGE_KEY = 'memes'
+const STORAGE_KEY1 = 'memesInfo'
+
+
+// saved memes page
+function renderSavedMemesPage() {
+    console.log('memespage');
+    var strHtml = []
+    gMemes = loadFromStorage(STORAGE_KEY)
+    const memesInfo = loadFromStorage(STORAGE_KEY1)
+    if (Array.isArray(gMemes)) {
+      gMemes.map((meme, idx) => {
+        strHtml.push(`<img src="${meme}" onclick="onSelectedMeme('${encodeURIComponent(JSON.stringify(memesInfo[idx]))}')">`);
+      })
+    } else {
+      strHtml.push('<h3>no saved memes</h3>')
+    }
+    document.querySelector('.saved-memes-container').innerHTML = strHtml.join('')
+  }
+  // show saved memes////
+  function onShowSavedMemesPage() {
+    documentActions('.home-page', 'hidden',  true)
+    documentActions('.meme-design', 'hidden',  true)
+    showSavedMemesPage()
+  }
+
+// show saved memes page
+  function showSavedMemesPage() {
+    renderSavedMemesPage()
+    documentActions('.saved-memes-page', 'hidden',  false)
+  }
+
+  // hide saved memes page
+function hideSavedMemesPage() {
+  documentActions('.saved-memes-page', 'hidden',  true)
+}
