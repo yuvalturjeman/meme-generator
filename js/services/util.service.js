2f610022e1a2c333cd4cc989d020d2f7e34b0102
yuvalturjeman <120467074+yuvalturjeman@users.noreply.github.com>
2023-04-29 16:52:11 +0300

    Update util.service.js

--- js/services/util.service.js
+++ js/services/util.service.js
@@ -1,8 +1,6 @@
 'use strict'
 
-
-
-function makeLorem(wordCount = 100) {
+function makeLorem(wordCount = 30) {
   const words = ['When your code is not functioning after 24 hours of debuging', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
   var txt = ''
   while (wordCount > 0) {
@@ -12,7 +10,7 @@ function makeLorem(wordCount = 100) {
   return txt
 }
 
-function makeLoremHe(wordCount = 100) {
+function makeLoremHe(wordCount = 30) {
   const words = ['השמיים', 'מעל', 'הנמל', 'היה', 'צבע של הטלוויזיה', 'מכון', 'אל', 'ערוץ מת', '.', 'כל', 'זה קרה', 'פחות או יותר', '.', 'אני', 'שייך', 'הסיפור', 'לאט לאט', 'מאנשים שונים', 'גם', 'בכלליות', 'קורה', 'במקרים כאלה', 'כל פעם', 'זה', 'היה', 'סיפור שונה', '.', 'זה', 'היה', 'תענוג', 'גם', 'בער']
   var txt = ''
   while (wordCount > 0) {
@@ -21,7 +19,6 @@ function makeLoremHe(wordCount = 100) {
   }
   return txt
 }
-
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min)
