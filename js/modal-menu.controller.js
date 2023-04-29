8132b360f64f3630bb22feaf0e7ef6ea50aff225
yuvalturjeman <120467074+yuvalturjeman@users.noreply.github.com>
2023-04-29 16:46:59 +0300

    Update modal-menu.controller.js

--- js/modal-menu.controller.js
+++ js/modal-menu.controller.js
@@ -1,8 +1,11 @@
 'use strict'
 
-function onToggleModal(){    
+function onToggleModal(){
+    var aboutMsg = makeLorem()
+    documentActions('.modal p','innerText',aboutMsg)
     document.body.classList.toggle('modal-open'); 
 }
+
 function onToggleMenu() {
     document.body.classList.toggle('menu-open');
     if (document.body.classList.contains('modal-open')) onToggleModal()
