95d5a0822e24bfb20a1941b444ef0536a89adc27
yuvalturjeman <120467074+yuvalturjeman@users.noreply.github.com>
2023-04-29 16:55:39 +0300

    Add files via upload

---
+++ js/canvas.controller.js
@@ -0,0 +1,112 @@
+'use strict'
+
+var gElCanvas
+
+function drawText(text, color, size, font = 'times', align, lineIdx) {
+    const meme = getMeme()
+    gCtx.lineWidth = 1
+    gCtx.strokeStyle = `white`
+    gCtx.fillStyle = `${color}`
+    gCtx.font = `${size}px ${font}`
+    gCtx.textAlign = `${align}`
+    gCtx.textBaseline = 'middle'
+    const x = meme.lines[lineIdx].x
+    const y = meme.lines[lineIdx].y
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+function onLineHeightChange(diff) {
+    const meme = getMeme()
+    const lineIdx = meme.selectedLineIdx
+    const line = getLine(lineIdx)
+    line.diff += diff
+    setTxtPos(lineIdx)
+    renderMeme()
+}
+
+function setTxtPos(lineIdx) {
+    const line = getLine(lineIdx)
+    switch (lineIdx) {
+        case 0:
+            line.x = 50
+            line.y = 50 + line.diff
+            if (line.y < 50) {
+                line.y = 50
+                line.diff = 0
+            }
+            else if (line.y >= gElCanvas.height - 50) {
+                line.y = gElCanvas.height - 50
+                line.diff = 0
+            }
+            break
+
+        case 1:
+            line.x = 150
+            line.y = gElCanvas.height - 50 + line.diff
+            if (line.y < 50) {
+                line.y = 50
+                line.diff = 0
+            }
+            else if (line.y >= gElCanvas.height - 50) {
+                line.y = gElCanvas.height - 50
+                line.diff = 0
+            }
+            break
+
+        default:
+            line.x = gElCanvas.width / 2
+            line.y = gElCanvas.height / 2 + line.diff
+            if (line.y < 50) {
+                line.y = 50
+                line.diff = 0
+            }
+            else if (line.y >= gElCanvas.height - 50) {
+                line.y = gElCanvas.height - 50
+                line.diff = 0
+            }
+            break
+    }
+}
+
+// add emoji to the img
+function addEmoji(elBtn) {
+    gMeme.lines.push({
+        txt: `${elBtn.innerText}`,
+        size: 50,
+        align: 'center',
+        color: 'black',
+        diff: 0,
+        x: gElCanvas.height / 2,
+        y: gElCanvas.width / 2,
+    })
+}
+
+function changeFontName(fontName) {
+    gMeme.font = fontName
+}
+
+function setColor(newColor) {
+    gMeme.lines[gMeme.selectedLineIdx].color = newColor
+}
+function changeFontSize(diff) {
+    gMeme.lines[gMeme.selectedLineIdx].size += diff
+}
+
+function getEmojis() {
+    const startIdx = gPageIdx * PAGE_SIZE
+    return gEmojis.slice(startIdx, startIdx + 3)
+}
+
+function getLineTxt() {
+    const idx = gMeme.selectedLineIdx
+    return gMeme.lines[idx].txt
+}
+
+function getLine(idx) {
+    return gMeme.lines[idx]
+}
+
+function getCurrLine() {
+    return gMeme.lines[gMeme.selectedLineIdx]
+}
